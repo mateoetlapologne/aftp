bbf90f73023e4c9781ed50e62be4f30829691c09
Bavrooo <salvador.mateo205@gmail.com>
2023-07-05 04:13:48 +0200

    sss

--- register.php
+++ register.php
@@ -1,32 +1,16 @@
 <?php
-// Fonction de hachage de mot de passe avec un salt aléatoire
-function hashMotDePasse($motdepasse) {
-    $salt = generateSalt(); // Génère un salt aléatoire
-    $hashed_password = sha1($motdepasse . $salt); // Applique le hachage avec le salt
-    return array("motdepasse" => $hashed_password, "salt" => $salt);
-}
-
-// Fonction pour générer un salt aléatoire
-function generateSalt($length = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $salt = '';
-    for ($i = 0; $i < $length; $i++) {
-        $salt .= $characters[rand(0, strlen($characters) - 1)];
-    }
-    return $salt;
-}
-
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les valeurs du formulaire
     $nom = $_POST["nom"];
+    $email = $_POST["email"];
     $motdepasse = $_POST["motdepasse"];
     
     // Connexion à la base de données
     $serveur = "localhost"; // Remplacez par le nom de votre serveur MySQL
-    $utilisateur = "root"; // Remplacez par votre nom d'utilisateur MySQL
-    $mdp = "667"; // Remplacez par votre mot de passe MySQL
-    $bdd = "aftp"; // Remplacez par le nom de votre base de données
+    $utilisateur = "nom_utilisateur"; // Remplacez par votre nom d'utilisateur MySQL
+    $mdp = "mot_de_passe"; // Remplacez par votre mot de passe MySQL
+    $bdd = "nom_bdd"; // Remplacez par le nom de votre base de données
     
     $connexion = mysqli_connect($serveur, $utilisateur, $mdp, $bdd);
     
@@ -38,14 +22,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Échapper les caractères spéciaux pour éviter les injections SQL
     $nom = mysqli_real_escape_string($connexion, $nom);
     $email = mysqli_real_escape_string($connexion, $email);
-    
-    // Hasher le mot de passe
-    $hashed_data = hashMotDePasse($motdepasse);
-    $motdepasse_hash = $hashed_data["motdepasse"];
-    $salt = $hashed_data["salt"];
+    $motdepasse = mysqli_real_escape_string($connexion, $motdepasse);
     
     // Créer la requête d'insertion
-    $requete = "INSERT INTO utilisateurs (pseudo, motdepasse, salt) VALUES ('$nom', '$motdepasse_hash', '$salt')";
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $requete = "INSERT INTO utilisateurs (pseudo, motdepasse, ip) VALUES ('$nom', '$motdepasse', $ip)";
     
     // Exécuter la requête d'insertion
     if (mysqli_query($connexion, $requete)) {
