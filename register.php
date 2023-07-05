5f99fa598eaee2902d896fe4c5801fa6d3ddc8da
Bavrooo <salvador.mateo205@gmail.com>
2023-07-05 03:55:16 +0200

    sqdqsd

--- register.php
+++ register.php
@@ -1,25 +1,9 @@
 <?php
-// Fonction de hachage de mot de passe avec un salt aléatoire
-function hashMotDePasse($motdepasse) {
-    $salt = generateSalt(); // Génère un salt aléatoire
-    $hashed_password = sha1($motdepasse . $salt); // Applique le hachage avec le salt
-    return array("motdepasse" => $hashed_password, "salt" => $salt);
-}
-
-// Fonction pour générer un salt aléatoire
-function generateSalt($length = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $salt = '';
-    for ($i = 0; $i < $length; $i++) {
-        $salt .= $characters[rand(0, strlen($characters) - 1)];
-    }
-    return $salt;
-}
-
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les valeurs du formulaire
-    $nom = $_POST["pseudo"];
+    $nom = $_POST["nom"];
+    $email = $_POST["email"];
     $motdepasse = $_POST["motdepasse"];
     
     // Connexion à la base de données
@@ -38,14 +22,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Échapper les caractères spéciaux pour éviter les injections SQL
     $nom = mysqli_real_escape_string($connexion, $nom);
     $email = mysqli_real_escape_string($connexion, $email);
+    $motdepasse = mysqli_real_escape_string($connexion, $motdepasse);
     
-    // Hasher le mot de passe
-    $hashed_data = hashMotDePasse($motdepasse);
-    $motdepasse_hash = $hashed_data["motdepasse"];
-    $salt = $hashed_data["salt"];
-    $ip = $_SERVER['REMOTE_ADDR'];
     // Créer la requête d'insertion
-    $requete = "INSERT INTO utilisateurs (pseudo, motdepasse, ip, salt) VALUES ('$pseudo','$motdepasse_hash', $ip, '$salt')";
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $requete = "INSERT INTO utilisateurs (pseudo, motdepasse, ip) VALUES ('$nom', '$motdepasse', '$ip')";
     
     // Exécuter la requête d'insertion
     if (mysqli_query($connexion, $requete)) {
@@ -79,8 +60,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <h2>Registration</h2>
     <form method="post">
-        <label for="nom">Pseudo</label>
-        <input type="text" name="pseudo" required><br><br>        
+        <label for="nom">Nom :</label>
+        <input type="text" name="nom" required><br><br>        
         <label for="motdepasse">Mot de passe :</label>
         <input type="password" name="motdepasse" required><br><br>
         
