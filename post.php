9af6007b6166138c940bcf8382f7f1d82e5b0e74
Bavrooo <salvador.mateo205@gmail.com>
2023-07-05 08:12:59 +0200

    qsdqsd

--- post.php
+++ post.php
@@ -80,6 +80,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Déplacement de l'image de la victime vers le dossier de destination
         $dossierDestination = "/var/www/html/aftp/image/";
         move_uploaded_file($_FILES["image"]["tmp_name"], $dossierDestination . $nomPhotoVictime);
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $dossierDestination . $nomPhotoVictime)) {
+            echo "L'image a été téléchargée avec succès.";
+        } else {
+            echo "Erreur lors du téléchargement de l'image.";
+        }
 
         // Génération de noms uniques pour les preuves et enregistrement dans la base de données
         $preuveNoms = array();
