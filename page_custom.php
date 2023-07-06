7ac5417cda79959af3be4f46c2c8a0d296b91374
Bavrooo <salvador.mateo205@gmail.com>
2023-07-06 14:12:23 +0200

    qsd

--- page_custom.php
+++ page_custom.php
@@ -20,16 +20,7 @@
     </style>
 </head>
 <body>
-<link rel="stylesheet" type="text/css" href="css/index.css">
-<div class="navbar">
-    <div class="left-button">
-        <a href="#" class="nav-title">Affiche Ton Pedo</a>
-    </div>
-    <div class="right-buttons">
-        <a href="post.php"><button class="rounded-button">Poster</button></a>
-        <a href="recherche.php"><button class="rounded-button">Rechercher</button></a>
-    </div>
-</div>
+    
     <?php
     // Connexion à la base de données
     $connexion = mysqli_connect("localhost", "root", "667", "aftp");
