ca0d6ad937d8c0c538ff33c8f845f449f9196426
Bavrooo <salvador.mateo205@gmail.com>
2023-07-07 15:59:29 +0200

    qsd

--- post.php
+++ post.php
@@ -83,35 +83,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             die("Erreur de connexion à la base de données : " . mysqli_connect_error());
         }
 
-// Génération d'un nom unique pour l'image recadrée
-$nomPhotoVictime = uniqid() . ". " . $imageExtension . "";
-
-// Récupérer les données de l'image recadrée
-$croppedImageData = $_POST['cropped_image'];
-
-// Supprimer l'en-tête de l'encodage base64
-$croppedImageData = str_replace('data:image/' . $imageExtension .';base64,', '', $croppedImageData);
-$croppedImageData = str_replace(' ', '+', $croppedImageData);
-
-// Décoder les données base64
-$decodedData = base64_decode($croppedImageData);
-
-// Créer une image à partir des données décodées
-$image = imagecreatefromstring($decodedData);
-
-// Chemin de destination pour l'enregistrement de l'image recadrée
-$dossierDestination = 'image/';
-$imagePath = $dossierDestination . $nomPhotoVictime;
-
-// Convertir et enregistrer l'image au format PNG
-if (imagepng($image, $imagePath)) {
-    echo "L'image recadrée a été convertie et enregistrée avec succès au format PNG.";
-    // Vous pouvez maintenant utiliser le fichier $imagePath qui contient l'image convertie en format PNG
-} else {
-    echo "Erreur lors de la conversion et de l'enregistrement de l'image recadrée.";
-}
-
-// Libérer la mémoire utilisée par l'image
+        // Génération d'un nom unique pour l'image recadrée
+        $nomPhotoVictime = uniqid() . $imageExtension;
+        
+        // Récupérer les données de l'image recadrée
+        $croppedImageData = $_POST['cropped_image'];
+        
+        // Supprimer l'en-tête de l'encodage base64
+        $croppedImageData = str_replace('data:image/jpeg;base64,', '', $croppedImageData);
+        $croppedImageData = str_replace(' ', '+', $croppedImageData);
+        
+        // Décoder les données base64
+        $decodedData = base64_decode($croppedImageData);
+        
+        // Créer une image à partir des données décodées
+        $image = imagecreatefromstring($decodedData);
+        
+        // Chemin de destination pour l'enregistrement de l'image recadrée
+        $dossierDestination = 'image/';
+        $imagePath = $dossierDestination . $nomPhotoVictime;
+        
+        // Enregistrer l'image recadrée au format PNG
+        if (imagepng($image, $imagePath)) {
+            echo "L'image recadrée a été convertie et enregistrée avec succès au format PNG.";
+        
+            // Chemin de destination pour l'upload de l'image
+            $dossierUpload = 'image/';
+        
+            // Déplacer l'image recadrée vers le dossier de destination
+            if (move_uploaded_file($imagePath, $dossierUpload . $nomPhotoVictime)) {
+                echo "L'image recadrée a été téléchargée avec succès.";
+            } else {
+                echo "Erreur lors du téléchargement de l'image recadrée.";
+            }
+        } else {
+            echo "Erreur lors de la conversion et de l'enregistrement de l'image recadrée.";
+        }
+        
+        // Libérer la mémoire utilisée par l'image
+        imagedestroy($image);
 
 
 
